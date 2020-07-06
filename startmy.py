d912c863ad2f87321b6c155fd9b7ea4354a58aba
SoumyajitMandal7 <67773197+SoumyajitMandal7@users.noreply.github.com>
2020-07-06 11:28:53 +0530

    Add files via upload

---
+++ startmy.py
@@ -0,0 +1,12 @@
+def line(n):
+    
+    for i in range(0, n):
+        
+        for j in range(0, i+1):
+            print("* ",end="")
+        print("\r")
+    
+n=int(input("Enter the no of lines  "))
+line(n)
+    
+   
